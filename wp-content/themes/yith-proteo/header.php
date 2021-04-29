96f69263c0a5a74c7f8d50f1d3f51b81121b65a5
Adrian Amaris <adrian@mutoestudio.com>
2021-04-29 00:58:20 -0500

    Menus

--- wp-content/themes/yith-proteo/header.php
+++ wp-content/themes/yith-proteo/header.php
@@ -13,7 +13,15 @@ global $post;
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+     <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-48JPY6JV41"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-48JPY6JV41');
+</script>
 	<?php wp_head(); ?>
 </head>
 
